5973d34c84e733eab0309564c2e208ba9fa0556e
Edoardo <ed13santi@gmail.com>
2024-03-06 19:32:16 +0000

    add histogram plotting function and renaming script

--- plot.py
+++ plot.py
@@ -1,5 +1,80 @@
 import pandas as pd
+import numpy as np
+import os
 import matplotlib.pyplot as plt
+import pickle
+
+def create_histogram(data_list, title="Histogram"):
+    """
+    Create a histogram for a given list of data.
+    """
+    plt.hist(data_list, bins=None, alpha=0.5)
+    plt.title(title)
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.show()
+
+max_e = lambda ll: max(max(sublist) for sublist in ll)
+min_e = lambda ll: min(min(sublist) for sublist in ll)
+
+def process_files_in_folder(folder_path):
+    """
+    Process files in a folder. Each file contains pickled data.
+    """
+    # Iterate through each file in the folder
+    betas = [i * 0.1 for i in range(0, 21)]
+
+    for beta in betas:
+        file_path0 = folder_path + folder_path[-2] + "_pomdp_aoi_" + str(beta)[:3]
+        file_path1 = folder_path + folder_path[-2] + "_politer_aoi_" + str(beta)[:3]
+
+        fig, axs = plt.subplots(2, 1)
+
+        # Check if the file is a regular file
+        if os.path.isfile(file_path0):
+            try:
+                with open(file_path0, 'rb') as file:
+                    loaded_data0 = pickle.load(file)  # Load pickled data from the file
+
+                    # If loaded data is a list, create a histogram
+                    if isinstance(loaded_data0, list):
+                        histogram_title = f'{file_path0} Histogram'
+                        axs[0].hist(loaded_data0, bins=np.arange(min_e(loaded_data0), max_e(loaded_data0)+1)-0.5)
+                        axs[0].set_title('histogram_title')
+                        axs[0].set_xlabel('Value')
+                        axs[0].set_ylabel('Frequency')
+                    else:
+                        print(f'Loaded data from {file_path0} is not a list.')
+            except Exception as e:
+                print(f'Error processing {file_path0}: {e}')
+
+        # Check if the file is a regular file
+        if os.path.isfile(file_path1):
+            try:
+                with open(file_path1, 'rb') as file:
+                    loaded_data1 = pickle.load(file)  # Load pickled data from the file
+
+                    # If loaded data is a list, create a histogram
+                    if isinstance(loaded_data1, list):
+                        histogram_title = f'{file_path1} Histogram'
+                        axs[1].hist(loaded_data1, bins=np.arange(min_e(loaded_data1), max_e(loaded_data1)+1)-0.5)
+                        axs[1].set_title(histogram_title)
+                        axs[1].set_xlabel('Value')
+                        axs[1].set_ylabel('Frequency')
+                    else:
+                        print(f'Loaded data from {file_path1} is not a list.')
+            except Exception as e:
+                print(f'Error processing {file_path1}: {e}')
+
+        
+            plt.tight_layout()
+            plt.show()
+
+# Specify the folder containing the files
+folder_path = 'results/1/'
+
+# Process files in the folder
+process_files_in_folder(folder_path)
 
 # Read the CSV file
 df = pd.read_csv('results/1/1_results.csv')
